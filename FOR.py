489d53e544ba1125078bb37317fb110f9b045b82
Evgeniy Kolesnikov <73242170+EKolesnikov@users.noreply.github.com>
2020-10-22 01:02:27 +0300

    Delete FOR.py

--- FOR.py
+++
@@ -1,9 +0,0 @@
-import random
-list = random.sample(range(10), 5)
-for x in list:
-    print(x)
-if 3 in list:
-    res = "the digit 3 is in the list under the # {}."
-    print(res.format(list.index(3)))
-else:
-    print("The digit 3 is missing")
