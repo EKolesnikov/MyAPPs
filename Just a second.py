8ad82b6a2d21186c8938a29e16e3fdff028e20e5
Evgeniy Kolesnikov <kolesnikov.e@hotmail.com>
2020-10-22 03:13:14 +0300

    new

---
+++ Just a second.py
@@ -0,0 +1,20 @@
+def timefunction():
+    a = int(input("Введите количество секунд:"))
+    d = (a//86400) % 24
+    h = (a // 3600) % 24
+    m = (a // 60) % 60
+    s = a % 60
+    if h < 10:
+        h = str('0'+str(h))
+    if m < 10:
+        m = str('0' + str(m))
+    else:
+        m = str(m)
+    if s < 10:
+        s = str('0' + str(s))
+    else:
+        s = str(s)
+    print(str(d) + ':' + str(h) + ':' + str(m) + ':' + str(s))
+
+
+timefunction()
